f35b348aee38221ff03ad4c8de57f6920eb562ef
TurkeyChans <louis.mercado192@gmail.com>
2024-03-04 22:39:46 -0500

    Initialize app state variables and cached element references

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+// Section 1: TODOs
+// TODO: Register submissions from the user on the form.
+// TODO: Determine the value of the data submitted and add it to a JavaScript array calle
+// TODO: Call the render function to update the table with the new tasks.
+// Section 2: App State Variables
+let tasks = [];
+// Section 3: Cached Element References
+const taskForm = document.getElementById("taskForm");
+const taskTable = document.getElementById("taskTable");
+console.log("ID = " + taskForm);
+console.log("ID = " + taskTable);
