811f3ac84b1a4328928dfcbe3862f929868c375b
TurkeyChans <louis.mercado192@gmail.com>
2024-03-07 17:52:05 -0500

    done with project

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 // TODO: Call the render function to update the table with the new tasks.
 // Section 2: App State Variables
 let tasks = [];
+let id_save = 0;
 // Section 3: Cached Element References
 const taskForm = document.getElementById("taskForm");
 const taskTable = document.getElementById("taskTable");
@@ -21,20 +22,25 @@ function handleSubmission(event) {
     if(taskName == "" || taskDescription == "" || taskDeadline == "") {
         alert('Task name and deadline are required!');
     }
-    // TODO: Update the tasks array
-    tasks.push({name: taskName, description: taskDescription, deadline: taskDeadline})
-    render();
+    else {
+        // TODO: Update the tasks array
+        tasks.push({name: taskName, description: taskDescription, deadline: taskDeadline})
+        render();
+    }
+    
 }
+
 // Function to render tasks in the table
 function render() {
     // TODO: Use array methods to create a new table row of data for each item in the arr
+    id_save = 0;
     taskTable.innerHTML = tasks.map(task => `
-        <tr>
-            <td>${task.name}</td>
-            <td>${task.description}</td>
-            <td>${task.deadline}</td>
-            <td><button onclick="markTaskComplete(this)">Complete</button></td>
-            <td><button onclick="removeTask(this)">Remove</button></td>
+        <tr id=id_${id_save++}>
+            <td><h5>Name: ${task.name}</h5></td>
+            <td><h5>Description: ${task.description}</h5></td>
+            <td><h5>Deadline: ${task.deadline}</h5></td>
+            <td><button onclick="markTaskComplete(${id_save})">Complete</button></td>
+            <td><button onclick="removeTask(${id_save})">Remove</button></td>
         </tr>
     `).join('');
    
@@ -43,7 +49,21 @@ function render() {
 function init() {
     taskTable.innerHTML = ''; // Clear the table
     tasks = []; // Reset the tasks array
+    id_save = 0;
     render(); // Call the render function
+}
+function markTaskComplete(num) {
+    num--;
+    const row = document.getElementById("id_" + num);
+    const img = document.createElement('img');
+    img.src = 'check.jpg';
+    img.style.height = '50px';
+    row.appendChild(img);
+}
+
+function removeTask(num) {
+    num--;
+    document.getElementById("id_" +num).remove();
 }
 taskForm.addEventListener('submit', handleSubmission);
 // Call the init function to set up the initial state of the app
